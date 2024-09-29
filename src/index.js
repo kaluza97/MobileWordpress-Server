3a03cc80baa5a479303c5b628a49256c5ac6ac87
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-09-29 18:19:35 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Connected to MongoDB Atlas'))
+    .catch((error) => console.error(error))
+
+app.listen(PORT, () => {
+    console.log(`Serwer działa na porcie ${PORT}`);
+});
