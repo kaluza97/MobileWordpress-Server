7acc0fc05d58a9463660701f0b5c835eb50ff74f
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-11-10 12:19:09 +0100

    improve view controllers

--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -25,32 +25,31 @@ export const createView = async (req: Request, res: Response): Promise<void> => 
 
 export const getViewById = async (req: Request, res: Response): Promise<void> => {
     try {
-        const view = await View.findById(req.body.id);
-
-        res.status(200).json(view)
-
-    }
-    catch (error) {
+        const view = await View.findById(req.params.id);
+        res.status(200).json(view);
+    } catch (error) {
         res.status(500).json({ message: 'Error fetching record.', error });
     }
 };
 
 export const updateViewById = async (req: Request, res: Response): Promise<void> => {
     try {
-        const view = await View.findOneAndUpdate(req.body.id, { name: req.body.name }, { new: true })
-        res.status(200).json(view)
-    }
-    catch (error) {
+        const view = await View.findOneAndUpdate(
+            { _id: req.params.id },
+            { name: req.body.name },
+            { new: true }
+        );
+        res.status(200).json(view);
+    } catch (error) {
         res.status(500).json({ message: 'Error updating record.', error });
     }
 };
 
 export const deleteViewById = async (req: Request, res: Response): Promise<void> => {
     try {
-        const view = await View.findOneAndDelete(req.body.id)
-        res.status(202).json(view)
-    }
-    catch (error) {
+        const view = await View.findOneAndDelete({ _id: req.params.id });
+        res.status(202).json(view);
+    } catch (error) {
         res.status(500).json({ message: 'Error deleting record.', error });
     }
 };
