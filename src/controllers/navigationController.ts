4e901fc28df896628525ae51e6256d45a26b4f21
pkaluzny <piotr.kaluzny@capgemini.com>
2024-11-12 14:48:07 +0100

    small refactor

--- src/controllers/navigationController.ts
+++ src/controllers/navigationController.ts
@@ -1,27 +1,48 @@
-import { Request, Response } from 'express';
-import Navigation from '../models/navigationModel';
+import { Request, Response } from "express";
+import Navigation from "../models/navigationModel";
 
-export const getNavigation = async (_req: Request, res: Response): Promise<void> => {
-    try {
-        const navigation = await Navigation.findOne();
+export const getNavigation = async (
+  _req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const navigation = await Navigation.findOne();
 
-        res.status(200).json(navigation);
-    }
-    catch (error) {
-        res.status(500).json({ message: 'Error fetching navigation.', error });
-    }
+    res.status(200).json(navigation);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching navigation.", error });
+  }
 };
 
-export const updateNavigation = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const updatedNavigation = await Navigation.findOneAndUpdate(
-            {},
-            { navigation: req.body.navigation },
-            { new: true, upsert: true }
-        );
+export const updateNavigation = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const updatedNavigation = await Navigation.findOneAndUpdate(
+      {},
+      { navigation: req.body.navigation },
+      { new: true, upsert: true }
+    );
 
-        res.status(200).json(updatedNavigation);
-    } catch (error) {
-        res.status(500).json({ message: 'Error updating navigation.', error });
-    }
+    res.status(200).json(updatedNavigation);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating navigation.", error });
+  }
+};
+
+export const clearNavigation = async (
+  _req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const clearedNavigation = await Navigation.findOneAndUpdate(
+      {},
+      { navigation: [] },
+      { new: true }
+    );
+    res.status(202).json(clearedNavigation);
+  } catch (error) {
+    res.status(500).json({ message: "Error clearing navigation.", error });
+  }
 };
--- src/routes/navigationRoutes.ts
+++ src/routes/navigationRoutes.ts
@@ -1,10 +1,16 @@
-import { Router } from 'express';
-import { getNavigation, updateNavigation } from '../controllers/navigationController';
+import { Router } from "express";
+import {
+  getNavigation,
+  updateNavigation,
+  clearNavigation,
+} from "../controllers/navigationController";
 
 const router = Router();
 
-router.get('/', getNavigation);
+router.get("/", getNavigation);
 
-router.patch('/', updateNavigation);
+router.patch("/", updateNavigation);
+
+router.patch("/", clearNavigation);
 
 export default router;
