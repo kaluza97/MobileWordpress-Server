4d601f9c5c858735abdb7e4d8ff59663abaa9840
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-11-21 02:41:45 +0100

    add get on start

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,24 @@ import dotenv from 'dotenv';
 import viewRoutes from './routes/viewRoutes';
 import navigationRoutes from './routes/navigationRoutes';
 import headerRoutes from './routes/headerRoutes';
-import { Router } from 'express';
 
 dotenv.config();
 
 const app: Application = express();
-const router = Router();
 
 app.use(cors());
 app.use(express.json());
-
-router.get('/', (_req, res) => {
-    res.send('Views API working!');
-});
-
 
 app.use('/api/views', viewRoutes);
 app.use('/api/settings/navigation', navigationRoutes);
 app.use('/api/settings/header', headerRoutes);
 
+app.get('/', (_req, res) => {
+    res.send('Your Node.js server is working properly!')
+})
+
 mongoose.connect(process.env.MONGODB_URI || '')
-    .then(() => console.log('Connected to MongoDB Atlas'))
+    .then(() => app.listen(9000, () => console.log("Server ready on port 9000.")))
     .catch((error: Error) => console.error(error));
 
 export default app;
