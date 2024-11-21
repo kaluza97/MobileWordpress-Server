3b4a7fe373d93c19b8668f2901b13027576354f0
kaluza97 <piotrkaluzny@MacBook-Pro-Piotr.local>
2024-11-21 01:23:33 +0100

    add export app for vercel

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/views', viewRoutes);
 app.use('/api/settings/navigation', navigationRoutes);
 app.use('/api/settings/header', headerRoutes);
@@ -20,5 +21,4 @@ mongoose.connect(process.env.MONGODB_URI || '')
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch((error: Error) => console.error(error));
 
-app.use(express.static("public"))
-
+export default app;
